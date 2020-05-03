0f51466356c0f12d5cc20188afdfa537d75817e7
BittahLover <maxx.angelvsdevil@gmail.com>
2020-05-03 16:45:27 +0300

    Django, first commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!E:\University\second-year\isp\prilozuhi\djangoStart\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
